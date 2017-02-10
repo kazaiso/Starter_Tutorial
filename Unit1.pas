f966374ad671b3128ed3f79675eba03cd42a2a0f
kazaiso <kazutaka.aiso@embarcadero.com>
2017-02-10 14:34:19 +0900

    move type definition from implementation to interface section

--- Unit1.pas
+++ Unit1.pas
@@ -24,6 +24,21 @@ type
     procedure Show(str: String);
   end;
 
+  /// // メソッド付きレコード定義のコード例/////
+type
+  TMyDate = record // TMyDateというレコード識別子の定義
+  private // 他unitからのアクセスを制限するキーワード private
+    Year: Integer; // 内部データ保持用のフィールド の定義。外部unitからのアクセスを制限
+    Month: Integer;
+    Day: Integer;
+    myDateTime: TDateTime;
+    procedure SetDatetime; // 内部処理用のprocedureの宣言外部unitからのアクセスを制限
+  public // 外部unitから使用させる場合のキーワード public
+    // type定義内ではメソッドの識別子定義のみ.メソッドの実装はimplementationブロックで行う
+    procedure SetValue(Y, M, D: Integer); // レコードの値セット用のProcedure識別子定義
+    function GetValue: TDateTime; // レコードの値取得用のfunction識別子定義
+  end;
+
 var
   Form1: TForm1;
 
@@ -31,7 +46,7 @@ implementation
 
 {$R *.fmx}
 
-////////////////配列のコピーのサンプル////////////////
+/// //配列のコピーのサンプル/////
 procedure TForm1.Button1Click(Sender: TObject);
 var
   DoutekiArray, DoutekiArrayCopy: array of Integer; // 動的配列変数宣言
@@ -53,11 +68,11 @@ begin
   // コピー元、コピー先の配列変数のインデックス[0]が示す値を確認
   Show('動的配列元[0]:' + DoutekiArray[0].ToString + ' / 動的配列コピー[0]:' +
     DoutekiArrayCopy[0].ToString);
-  Show('静的配列元[0]:' + SeitekiArray[0].ToString +' / 静的配列コピー[0]:' +
+  Show('静的配列元[0]:' + SeitekiArray[0].ToString + ' / 静的配列コピー[0]:' +
     SeitekiArrayCopy[0].ToString);
 end;
 
-/// ///////ポインタ変数の使用とポインタ変数を使った値の変更////////////////////
+/// //ポインタ変数の使用とポインタ変数を使った値の変更/////
 procedure TForm1.Button2Click(Sender: TObject);
 var
   IPointer: ^Integer; // Integer型のポインタを保持する変数識別子を宣言
@@ -70,25 +85,10 @@ begin
 
   // ポインタを保持する変数識別子の後ろに ^  をつけることで実際の値の場所を意味する
   IPointer^ := 20; // ポインタ変数が参照している実データの場所に 値20を代入
-  Show('ポインタによって代入されたMyInt: '+ MyInt.ToString); // メッセージとして 20 が表示される
+  Show('ポインタによって代入されたMyInt: ' + MyInt.ToString); // メッセージとして 20 が表示される
 end;
 
-/// /////////////////メソッド付きレコード////////////////////////////////////
-// レコード定義のコード例
-type
-  TMyDate = record // TMyDateというレコード識別子の定義
-  private // 他unitからのアクセスを制限するキーワード private
-    Year: Integer; // 内部データ保持用のフィールド の定義。外部unitからのアクセスを制限
-    Month: Integer;
-    Day: Integer;
-    myDateTime: TDateTime;
-    procedure SetDatetime; // 内部処理用のprocedureの宣言外部unitからのアクセスを制限
-  public // 外部unitから使用させる場合のキーワード public
-    // type定義内ではメソッドの識別子定義のみ.メソッドの実装はimplementationブロックで行う
-    procedure SetValue(Y, M, D: Integer); // レコードの値セット用のProcedure識別子定義
-    function GetValue: TDateTime; // レコードの値取得用のfunction識別子定義
-  end;
-
+/// //メソッド付きレコードの使用例/////
 procedure TMyDate.SetDatetime;
 // レコード定義識別子.(ドット)メソッド定義識別子の形式で実装。TMyDataレコード定義のSetDateimeメソッド実装
 begin
@@ -116,11 +116,12 @@ procedure TForm1.Button3Click(Sender: TObject);
 var
   MyBirthday: TMyDate; // 上記で定義、実装しているメソッド付きTMyDataレコード型の変数識別子MyBirthdayを宣言
 begin
-  MyBirthday.SetValue(1995, 2, 14); //メソッド使用時はレコード変数識別子.メソッド名 で使用。
-  Show('メソッドでデータを取得: '+ DateTimeToStr(MyBirthday.GetValue)); //メソッド使用時は レコード変数識別子.メソッド名で使用。
+  MyBirthday.SetValue(1995, 2, 14); // メソッド使用時はレコード変数識別子.メソッド名 で使用。
+  Show('メソッドでデータを取得: ' + DateTimeToStr(MyBirthday.GetValue));
+  // メソッド使用時は レコード変数識別子.メソッド名で使用。
 end;
 
-////////////ウインドウ下部のメモにテキストを表示するだけのProcedure///////
+/// //ウインドウ下部のメモにテキストを表示するだけのProcedure/////
 procedure TForm1.Show(str: String);
 begin
   Memo1.Lines.Add(str);
